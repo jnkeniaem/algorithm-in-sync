95d6062bdfcde8b3cae94b8947199a0ad3f47a9a
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2024-11-20 08:25:38 +0900

    [level 2] Title: 튜플, Time: 100.15 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,58 +1,58 @@
 function solution(s) {
-    let map = new Map(); // 순서 보장해야됨
-    let ary = Array.from(s);
-    let isMatched = false;
-    ary.shift();
-    ary.pop();
-    let elemAry = [];
-    let tmpAry = [];
-    let numAry = [];
-    for (const elem of ary) {
-        if (elem == "}") {
-            isMatched = true;
-            elemAry.push(tmpAry);
-            tmpAry = [];
-            isMatched = false;
-        }
+  // {{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}}
+  let ary = Array.from(s); // s의 모든 문자를 배열에 담기
+  //   ['{','{','2','}','{','2',',','1','}', ... }]
+  ary.shift();
+  ary.pop();
+  // 맨 앞 맨 뒤의 중괄호 제거
+  //   ['{','2','}','{','2',',','1','}', ... , '}']
+  let elemAry = [];
+  let tmpAry = [];
+  let numAry = [];
+  let map = new Map();
 
-        if (!isMatched){
-            if (elem == ",")
-              tmpAry.push(elem);
-                
-            let num = Number(elem);
-            if (!isNaN(num))
-              tmpAry.push(num);
-        }
+  for (const elem of ary) {
+    if (elem == "}") {
+      // 집합의 끝 일때
+      elemAry.push(tmpAry);
+	// console.log("tmpAry : ",tmpAry);
+        
+      tmpAry = [];
+      continue;
     }
-    // console.log(Number(','));
-    for (const elem of elemAry) {
-        let str = ""
-        for (let i = 0; i < elem.length; ++i) {
-            if (elem[i] != ",") {
-                str += elem[i];
-                if (i + 1 == elem.length) {
-                    numAry.push(Number(str));
-                }
-            }
-            else {
-                numAry.push(Number(str));
-                str = ""                
-            }
-                
-        }
 
+	if (elem == ",") tmpAry.push(elem);
+
+    else if (elem != "{") tmpAry.push(elem);
+    //  '1','1','1' 같이 한자리수 이상 숫자들도 다룰 수 있게 ','도 넣기
+  }
+  // console.log("elemAry : ",elemAry);
+
+  for (const elem of elemAry) {
+    let str = "";
+    for (let i = 0; i < elem.length; ++i) {
+      if (elem[i] != ",") {
+        str += elem[i];
+        if (i + 1 == elem.length) {
+          numAry.push(Number(str));
+        }
+      } else {
+        numAry.push(Number(str));
+        str = "";
+      }
     }
-    
-    
-    for (const elem of numAry) {
-        map.set(elem, (map.get(elem) || 0) + 1);
-    }
-    map.delete(0);
-    let answer = new Array(map.size).fill(0);
-    
-    for (const [key, val] of map) {
-        answer[map.size - val] = key
-    }
-    
-    return answer;
+  }
+  // console.log("numAry : ",numAry);
+
+  for (const elem of numAry) {
+    map.set(elem, (map.get(elem) || 0) + 1);
+  }
+  map.delete(0);
+  let answer = new Array(map.size).fill(0);
+
+  for (const [key, val] of map) {
+    answer[map.size - val] = key;
+  }
+
+  return answer;
 }
