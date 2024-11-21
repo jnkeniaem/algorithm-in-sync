975500380f13f393b8edf3446223adbc2090fff9
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2024-11-21 23:42:43 +0900

    Time: 0 ms (100%), Space: 49.8 MB (61.27%) - LeetHub

--- LeetCode/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ LeetCode/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,43 +3,28 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
-    let ret = [-1, -1];
-    if (!nums.length)
-        return ret;
-    let len = nums.length;
-    let start = 0;
-    let end = len -1;
-    if (target < nums[start] || target > nums[end])
-        return ret;
-    
-    while (1) {
-        if (start > end) return ret;
+var searchRange = function (nums, target) {
+  let ret = [-1, -1];
+  if (!nums.length) return ret;
 
-        let middle = Math.floor((start + end) / 2);
-        // console.log(start, end, middle);
+  let len = nums.length;
+  let [start, end] = [0, len - 1];
+  if (target < nums[start] || target > nums[end]) return ret;
 
-        if (target > nums[middle]) {
-            start = middle + 1;
-        }
-        else if (target < nums[middle]) {
-            end = middle -1;
-        }
-        else {
-            start = middle;
-            end = middle;
-            while (nums[start] == target)
-                start--;
-            while (nums[end] == target)
-                end++;
-            // console.log(start + 1, end -1);
-            return [start + 1, end -1];
-            // 양쪽
-            // 왼쪽
-            // 오른쪽
-        }
-        // start, end 다 유효한지 범위 안에 있는지 확인
-        // start >= end 일때 finish
+  while (1) {
+    if (start > end) return ret;
 
+    let middle = Math.floor((start + end) / 2);
+
+    if (target > nums[middle]) {
+      start = middle + 1;
+    } else if (target < nums[middle]) {
+      end = middle - 1;
+    } else {
+      [start, end] = [middle, middle];
+      while (nums[start] == target) start--;
+      while (nums[end] == target) end++;
+      return [start + 1, end - 1];
     }
+  }
 };
