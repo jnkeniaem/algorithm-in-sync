99301affeb17f582efd0f0657542ed30c5217996
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2024-11-26 23:40:25 +0900

    Time: 5 ms (75.73%), Space: 54.5 MB (90.86%) - LeetHub

--- LeetCode/1306-jump-game-iii/1306-jump-game-iii.js
+++ LeetCode/1306-jump-game-iii/1306-jump-game-iii.js
@@ -3,27 +3,21 @@
  * @param {number} start
  * @return {boolean}
  */
-var canReach = function(arr, start) {
-    let ret = false;
-    if (arr.indexOf(0) == -1)
-        return ret;
-    let visited = new Array(arr.length + 1).fill(false);
-    let q = [start];
-    // let cur = 0;
-    while (q.length) {
-    // while (cur <= q.length -1) {
-        let front = q.shift();
-        // let front = q[cur];
-        if (arr[front] == 0)
-            return true;
-        visited[front] = true;
-        let [forward, backward] = [front - arr[front], front + arr[front]]
-        if (!visited[forward] && forward >= 0)
-            q.push(forward);
-        if (!visited[backward] && backward < arr.length)
-            q.push(backward);
-        // cur++;
-    }
+var canReach = function (arr, start) {
+  if (arr.indexOf(0) == -1) return false;
+  let visited = new Array(arr.length + 1).fill(false);
+  let q = [start];
 
-    return false;
+  while (q.length) {
+    let front = q.shift();
+    if (arr[front] == 0) return true;
+
+    visited[front] = true;
+	
+    let [forward, backward] = [front - arr[front], front + arr[front]];
+    if (!visited[forward] && forward >= 0) q.push(forward);
+    if (!visited[backward] && backward < arr.length) q.push(backward);
+  }
+
+  return false;
 };
