d02855af650e85d761294480ff5ca5a7736b10a3
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-09-20 19:42:08 +0900

    Time: 5 ms (75.4%), Space: 62.6 MB (93.62%) - LeetHub

--- LeetCode/0997-find-the-town-judge/0997-find-the-town-judge.js
+++ LeetCode/0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -18,20 +18,11 @@ var findJudge = function (n, trust) {
     bAry[b] += 1;
   }
 
-  // 각 배열 돌면서
-  const nAry = [];
-
-  aAry.map((elem, idx) => {
-    // 1. a에 없는 사람, - false인 사람.
-    if (elem === false) nAry.push(idx);
-  });
-
-  if (nAry.length === 0) return -1; // 다 누군가를 믿고 있는 것
-  for (const elem of nAry) {
-    // 2. 걸러진 사람들 중 다른 사람들(n-1명)은 ai를 믿어야 함(bi 개수가 n-1이여야함) - findIndex bAry 요소가 n - 1인 사람.
-    if (bAry[elem] === n - 1) {
-      return elem;
+  for (let i = 1; i < aAry.length; ++i) {
+    if (aAry[i] === false && bAry[i] === n - 1) {
+      return i;
     }
   }
+
   return -1;
 };
