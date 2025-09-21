867f20e5bc4abb102850c8156a4ecc660fc5abf5
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-09-21 15:27:32 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+  if (n === 1)
+    return true;
+  const visited = new Array(n).fill(false);
+  const neighbors = new Array(n).fill([]).map(() => new Array());
+
+  for (const [u, v] of edges) {
+    // edges 순회하면서 양쪽 다 넣어주기
+    neighbors[u].push(v);
+    neighbors[v].push(u);
+  }
+
+  const q = [source];
+
+  while (q.length) {
+    const vertex = q.shift();
+    visited[vertex] = true;
+    const curNeighbors = neighbors[vertex];
+
+    for (const neighbor of curNeighbors) {
+      if (neighbor === destination) return true;
+      if (visited[neighbor] === false) q.push(neighbor);
+    }
+  }
+  return false;
+};
