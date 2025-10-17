25e83354c1c7d27440c70047d783e2489992d295
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-10-17 16:40:08 +0900

    Time: 19 ms (25.96%), Space: 62.1 MB (60.41%) - LeetHub

--- LeetCode/0207-course-schedule/0207-course-schedule.js
+++ LeetCode/0207-course-schedule/0207-course-schedule.js
@@ -1,45 +1,42 @@
-/**
- * @param {number} numCourses
- * @param {number[][]} prerequisites
- * @return {boolean}
- */
-var canFinish = function(numCourses, prerequisites) {
+var canFinish = function (numCourses, prerequisites) {
   // 1. 진입차수가 0인 것 찾아서 q에 넣기
   //  이때 없으면 return false
   // 2. 큐에서 노드를 꺼내서 연결된 간선 끊기
   // 3. 이후 진입로가 없는 것들 q에 넣기
-  const degrees = new Array(numCourses).fill(0);
-  const nexts = new Array(numCourses).fill([]).map(() => []);
-  // const preRs = new Array(numCourses).fill([]).map(() => []); // set로
-  const preRsSet = new Array(numCourses).fill(new Set()).map(() => new Set()); // set로
+  //   const levels = new Array(numCourses).fill(0);
+  const parents = new Array(numCourses).fill(new Set()).map(() => new Set()); // set로
+  // 전에 들어야 할 것
+  const children = new Array(numCourses).fill([]).map(() => []);
+  // 바로 다음에 들을 것
 
   for (const [a, b] of prerequisites) {
-    preRsSet[a].add(b);
-    degrees[a]++;
-    nexts[b].push(a);
+    parents[a].add(b);
+    // levels[a]++;
+    children[b].push(a);
   }
 
-  const q = []; // no entry
+  const q = []; // no entry nodes
 
-  //   for (let i = 0; i < preRsSet.length; ++i) {
-  //     if (preRsSet[i].length === 0) q.push(i);
-  //   }
-  for (let i = 0; i < degrees.length; ++i) {
-    if (degrees[i] === 0) q.push(i);
+  //   for (let i = 0; i < levels.length; ++i) {
+  // if (levels[i] === 0) q.push(i);
+  for (let i = 0; i < numCourses; ++i) {
+    if (parents[i].size === 0) q.push(i);
   }
 
   if (q.length === 0) return false;
+
   let cnt = 0;
   while (q.length) {
     const node = q.shift();
 
-    for (const next of nexts[node]) {
-      preRsSet[next].delete(node);
-      degrees[next]--;
-      if (degrees[next] === 0) q.push(next);
+    for (const child of children[node]) {
+      parents[child].delete(node);
+      //   levels[child]--;
+      //   if (levels[child] === 0) q.push(child);
+      if (parents[child].size === 0) q.push(child);
     }
-    //     // nexts 돌면서
-    //     // preRsSet 중 node 가지고 있는 것 다 간선 쳐내기
+    // children 돌면서
+    // parents 중 node 가지고 있는 것 다 간선 쳐내기
     cnt++;
   }
 
