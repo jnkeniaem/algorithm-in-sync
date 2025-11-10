01dbfa6ab5d8cc42bcb0a7f1d0547e83fcb4f6db
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-11-10 15:30:06 +0900

    [level 2] Title: 디펜스 게임, Time: 6590.85 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
@@ -1,71 +1,25 @@
-// function solution(n, k, enemy) {
-//   const totalRound = enemy.length;
-//   if (k >= totalRound) return totalRound;
-
-//   let answer = 0;
-//   let leftSkill = k; // 무적권 남은 횟수
-//   let curSoldiers = n;
-//   let q = [];
-
-//   for (let i = 0; i < totalRound; ++i) {
-//     const curEnemy = enemy[i];
-
-//     q.push(curEnemy);
-//     q.sort((x, y) => x - y); // 이게 엄청 오래 걸릴것임
-
-//     curSoldiers -= curEnemy;
-//     while (curSoldiers < 0) {
-//       if (leftSkill) {
-//         const max = q.pop();
-//         curSoldiers += max;
-//         leftSkill--;
-//       } else return answer;
-//     }
-//     answer++;
-//   }
-
-//   return answer;
-// }
-
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
 function solution(n, k, enemy) {
-    let [left, right] = [0, enemy.length];
+  //   const totalRound = enemy.length;
+  //   if (k >= totalRound) return totalRound;
+
+  //   let [left, right] = [0, totalRound - 1];
+  //   let [left, right] = [0, enemy.length - 1];
+  let [left, right] = [0, enemy.length];
+
+  while (left <= right) {
     let mid = Math.floor((left + right) / 2);
+    let leftSkill = k; // 무적권 남은 횟수
+    const curEnemies = enemy.slice(0, mid).sort((x, y) => y - x);
+    const curEnemy = curEnemies.reduce((acc, curVal) => {
+      if (leftSkill) {
+        leftSkill--;
+        return acc;
+      }
+      return acc + curVal;
+    }, 0);
+    if (n < curEnemy) right = mid - 1;
+    else left = mid + 1;
+  }
 
-    while (left <= right) {
-        const round = enemy.slice(0, mid).sort((a, b) => b - a);
-        let fever = k;
-        const remain = round.reduce((acc, val) => {
-            if (fever > 0) {
-                fever--;
-                return acc;
-            } else return acc + val
-        }, 0);
-        // console.log(round, fever, remain, mid)
-        if (n - remain >= 0 && fever >= 0) left = mid + 1;
-        else right = mid - 1;
-        mid = Math.floor((left + right) / 2);
-    }
-
-    return left - 1;
+  return left - 1;
 }
