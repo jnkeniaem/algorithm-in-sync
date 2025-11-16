f1b72d1cef9692674d7d02289ccb4c9bed6ad86b
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-11-16 19:52:27 +0900

    [level 2] Title: 주식가격, Time: 26.60 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,25 +1,16 @@
 function solution(prices) {
-  const answer = [];
+  const answer = prices.map((_, idx) => prices.length - 1 - idx);
 
-  for (let j = 0; j < prices.length - 1; ++j) {
-    // let period = 0;
-    let k = j + 1;
-    // while (k < prices.length && prices[j] <= prices[k]) k++;
-    while (k < prices.length) {
-      if (prices[j] <= prices[k]) k++;
-      else break;
+  const stack = [0];
+
+  for (let i = 1; i < prices.length; ++i) {
+    while (stack.length && prices[stack[stack.length - 1]] > prices[i]) {
+      const top = stack.pop();
+      answer[top] = i - top;
     }
-
-    // for (let k = j + 1; k < prices.length && curPrice > prices[k]; ++k) {
-    // if (curPrice <= prices[k]) period++;
-    // }
-    // answer.push(period);
-    if (k === prices.length) {
-      // 끝까지 안떨어짐
-      answer.push(k - 1 - j);
-    } else answer.push(k - j);
+    stack.push(i);
   }
+  // stack엔 시점이 들어감
 
-  answer.push(0);
   return answer;
 }
