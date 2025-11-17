bac2924cb1665165023b2b59c11e7ccc5efee0da
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-11-17 14:23:48 +0900

    [level 2] Title: 택배상자, Time: 28.41 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,55 +1,27 @@
 function solution(order) {
   let answer = 0;
+  let curBox = 0;
+  const subConveyer = [];
 
-  const convey = [...order].map((_, idx) => order.length - idx);
-
-  const subConvey = [];
-  for (let i = 0; i < order.length; ++i) {
-    /*
-    - subConvey && subConvey pop해서 top이 타겟 원하는 건지 확인
-        - o -> answer++; continue
-        - x ->
-          - 다시 subConvey에 push하고,
-          - 나올때가지 convey && topConvey !== target
-            - convey pop
-            - subConvey push
-            if (topConvey === target)
-            - continue
-            else
-            - return answer;
-
-    */
-    const target = order[i];
-    let topSubConvey = 0;
-    if (subConvey.length) {
-      topSubConvey = subConvey.pop();
-
-      if (topSubConvey === target) {
-        answer++;
-        continue;
-      }
-      subConvey.push(topSubConvey);
+  for (const target of order) {
+    while (curBox < target) {
+      subConveyer.push(curBox);
+      curBox++;
     }
 
-    let topConvey = 0;
-    if (convey.length) {
-      topConvey = convey.pop();
-
-      while (convey.length && topConvey !== target) {
-        subConvey.push(topConvey);
-        topConvey = convey.pop();
-        /*
-        원하는게 나올때까지 pop 해서 subConvey에 넣어주기
-        */
-      }
-      if (topConvey === target) {
-        // 원하는게 나온 것
-        answer++;
-        continue;
-      }
+    if (curBox === target) {
+      answer++;
+      curBox++;
+      continue;
     }
-    return answer;
+
+    if (subConveyer.length && subConveyer[subConveyer.length - 1] === target) {
+      subConveyer.pop();
+      answer++;
+    } else return answer;
   }
+  // while (curBox < order.length) {}
+  // 메인 컨베이어 벨트를 실제로 움직일 필요는 없다
 
   return answer;
 }
