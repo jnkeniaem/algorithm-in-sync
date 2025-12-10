c0c2abf338af5fc88d3b2daedf9e98464f1bd68c
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-12-10 12:58:00 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 17.07 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,56 +1,33 @@
 function solution(sequence, k) {
-  const kIdx = sequence.indexOf(k);
-  if (kIdx !== -1) return [kIdx, kIdx];
-
-  // const sequenceUnderK = sequence.filter((elem) => elem < k); // k 미만의 원소만 가지게
-  const candidate = [-1, -1]; // 후보 sIdx, 길이 sIdx, length
-  // 정확히 일치하는 원소 찾기
-
-  // const updateCandidate = (newLen, sIdx) => {
-  //   candidate.len = newLen;
-  //   candidate.sIdx = sIdx;
-  // };
+  const candidate = [-1, -1];
   let [sIdx, eIdx] = [0, 0];
-
   const seqLen = sequence.length;
   let sum = sequence[0];
 
-  /*
-  - sIdx~eIdx 값 < sum 반복해서 eIdx++
-  - 같으면
-      - answer 업데이트
-  - 더 크면
-      - sIdx++
-  */
-
-  while (sIdx < seqLen && eIdx < seqLen) {
+  while (eIdx < seqLen) {
     if (sum < k) {
       eIdx++;
-      sum += sequence[eIdx];
-    } else if (sum === k) {
-      // 비교해서 갱신
-      if (candidate[0] === -1) {
-        candidate[0] = sIdx;
-        candidate[1] = eIdx;
-      } else {
-        const prevLen = candidate[1] - candidate[0];
-        if (prevLen > eIdx - sIdx) {
-          // 길이
-          // 갱신
+      if (eIdx < seqLen) sum += sequence[eIdx];
+    } else {
+      if (sum === k) {
+        // 비교해서 갱신
+        if (candidate[0] === -1) {
           candidate[0] = sIdx;
           candidate[1] = eIdx;
-        } else if (prevLen === eIdx - sIdx) {
-          // 시작 인덱스
-          if (candidate[0] > sIdx) {
-            // 갱신
+        } else {
+          const prevLen = candidate[1] - candidate[0];
+          const curLen = eIdx - sIdx;
+          if (prevLen > curLen) {
+            // 길이
+            candidate[0] = sIdx;
+            candidate[1] = eIdx;
+          } else if (prevLen === curLen && candidate[0] > sIdx) {
+            // 시작 인덱스
             candidate[0] = sIdx;
             candidate[1] = eIdx;
           }
         }
       }
-      sum -= sequence[sIdx];
-      sIdx++;
-    } else {
       sum -= sequence[sIdx];
       sIdx++;
     }
