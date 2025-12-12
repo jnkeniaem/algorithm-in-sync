6476719e8a6f48ac169537522a004992a32a3fd1
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-12-12 11:43:59 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,32 +1,33 @@
 function solution(storey) {
   let answer = 0;
-  const storeyInStr = storey.toString();
-  const storeyLen = storeyInStr.length;
 
-  for (let i = 0; i < storeyLen; ++i) {
-    const digit = (storey % 10 ** (i + 1)) / 10 ** i;
+  for (let i = 0; storey > 0; ++i) {
+    // const digit = (storey % 10 ** (i + 1)) / 10 ** i;
+    const digit = storey % 10;
+    // console.log("digit : ", digit);
     if (digit < 5) {
       // 정석대로 빼기
-      storey -= digit * 10 ** i;
+      // storey -= digit * 10 ** i;
       answer += digit;
-      // 실제로 안빼도 됨
     } else if (digit > 5) {
       // 더해서 10의 배수로 만들기
-      storey += (10 - digit) * 10 ** i;
+      // storey += (10 - digit) * 10 ** i;
+      storey += 10 - digit;
       answer += 10 - digit;
     } else {
-      // 앞의 자리수를 보고 더해줬을때 6이상이 될것같으면 더해주기
       // 앞의 자리수가 5 이상이면 더해주기
-      const prevDigit = (storey % 10 ** (i + 2)) / 10 ** (i + 1);
-      if (prevDigit >= 5) {
-        storey += (10 - digit) * 10 ** i;
-        answer += 10 - digit;
-      } else {
-        storey -= digit * 10 ** i;
-        answer += digit;
-      }
+      // const prevDigit = (storey % 10 ** (i + 2)) / 10 ** (i + 1);
+      const prevDigit = Math.floor((storey % 100) / 10);
+      answer += 5;
+      // if (prevDigit) storey += 5 * 10 ** i;
+      // if (prevDigit >= 5) storey += 5 * 10;
+      if (prevDigit >= 5) storey += 5;
+      // storey += prevDigit >= 5 ? 5 * 10 ** i : -5 * 10 ** i;
     }
+    // console.log("storey: ", storey);
+    storey = Math.floor(storey / 10);
+    // console.log("after  storey: ", storey);
   }
 
-  return storey ? answer + 1 : answer;
+  return answer;
 }
