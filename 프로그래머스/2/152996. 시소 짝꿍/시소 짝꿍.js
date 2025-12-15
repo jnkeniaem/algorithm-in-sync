e2487494f7125e8167dfa9125c476e0c934b6013
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-12-15 12:54:47 +0900

    [level 2] Title: 시소 짝꿍, Time: 21.86 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,43 +1,39 @@
-
 function solution(weights) {
-  const map = new Map(); // 곱했을때 몸무게
-  const map2 = new Map();
+  const torqueMap = new Map();
+  const weightMap = new Map();
   const distances = [2, 3, 4];
   let answer = 0;
 
   for (let i = 0; i < weights.length; ++i) {
-    let val = map2.get(weights[i]);
-    if (val === undefined) val = 0;
-    map2.set(weights[i], val + 1);
+    // let val = weightMap.get(weights[i]);
+    // if (val === undefined) val = 0;
+    // weightMap.set(weights[i], val + 1);
+    weightMap.set(weights[i], (weightMap.get(weights[i]) || 0) + 1);
 
-    // for (const distance of distances) {
-    //   let val = map.get(weights[i] * distance);
-    //   if (val === undefined) {
-    //     map.set(weights[i] * distance, []);
-    //   }
-    //   val.push(i);
-    //   map.set(weights[i] * distance, [...val, i]);
-    // }
     for (const distance of distances) {
-      let val = map.get(weights[i] * distance);
-      if (val === undefined) {
-        val = 0;
-      }
-      map.set(weights[i] * distance, val + 1);
+      // let val = torqueMap.get(weights[i] * distance);
+      // if (val === undefined) {
+      //   val = 0;
+      // }
+      // torqueMap.set(weights[i] * distance, val + 1);
+      torqueMap.set(
+        weights[i] * distance,
+        (torqueMap.get(weights[i] * distance) || 0) + 1
+      );
     }
   }
 
-
-  for (const [weight, cnt] of map) {
+  for (const [weight, cnt] of torqueMap) {
     if (cnt > 1) {
       answer += (cnt * (cnt - 1)) / 2;
       // nC2
     }
   }
-  for (const [weight, cnt] of map2) {
+
+  for (const [weight, cnt] of weightMap) {
     if (cnt > 1) {
       // 중복 처리
-      answer -= cnt * (cnt - 1);
+      answer -= ((cnt * (cnt - 1)) / 2) * (distances.length - 1);
       // nC2
     }
   }
