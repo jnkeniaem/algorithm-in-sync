e06072c546631ab2287ecfd09f8c2a776ebaafe5
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-12-15 11:45:28 +0900

    [level 2] Title: 시소 짝꿍, Time: 42.95 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -0,0 +1,47 @@
+
+function solution(weights) {
+  const map = new Map(); // 곱했을때 몸무게
+  const map2 = new Map();
+  const distances = [2, 3, 4];
+  let answer = 0;
+
+  for (let i = 0; i < weights.length; ++i) {
+    let val = map2.get(weights[i]);
+    if (val === undefined) val = 0;
+    map2.set(weights[i], val + 1);
+
+    // for (const distance of distances) {
+    //   let val = map.get(weights[i] * distance);
+    //   if (val === undefined) {
+    //     map.set(weights[i] * distance, []);
+    //   }
+    //   val.push(i);
+    //   map.set(weights[i] * distance, [...val, i]);
+    // }
+    for (const distance of distances) {
+      let val = map.get(weights[i] * distance);
+      if (val === undefined) {
+        val = 0;
+      }
+      map.set(weights[i] * distance, val + 1);
+    }
+  }
+
+
+  for (const [weight, cnt] of map) {
+    if (cnt > 1) {
+      answer += (cnt * (cnt - 1)) / 2;
+      // nC2
+    }
+  }
+  for (const [weight, cnt] of map2) {
+    if (cnt > 1) {
+      // 중복 처리
+      answer -= cnt * (cnt - 1);
+      // nC2
+    }
+  }
+
+  // 마지막에 중복 처리
+  return answer;
+}
