8767c353076024eec28d8321b54cab4634520195
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-12-18 16:17:05 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.71 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,6 +1,6 @@
 function solution(n, wires) {
   let answer = 100;
-  const adjacents = new Array(n + 1).fill([]).map((_) => new Array().fill([])); 
+  const adjacents = new Array(n + 1).fill([]).map((_) => new Array().fill([]));
 
   // 각 송전탑의 연결 현황 구하기
   for (let i = 0; i < wires.length; ++i) {
@@ -10,27 +10,27 @@ function solution(n, wires) {
   }
 
   const visited = new Array(n + 1).fill(false);
-  let nodeCnt = 0;
 
   const getNodes = (root) => {
-    visited[root] = true;
     const value = adjacents[root];
+    let nodeCnt = 1;
+
+    visited[root] = true;
+
     for (const node of value) {
       if (visited[node] === false) {
         visited[node] = true;
-        nodeCnt++;
-        getNodes(node);
+        nodeCnt += getNodes(node);
       }
     }
+
+    return nodeCnt;
   };
 
   for (const [v1, v2] of wires) {
     // 하나씩 끊어보기 시뮬레이션
     visited[v1] = true;
-    nodeCnt = 1; // 본인
-    getNodes(v2);
-
-    answer = Math.min(answer, Math.abs(n - nodeCnt - nodeCnt));
+    answer = Math.min(answer, Math.abs(n - 2 * getNodes(v2)));
     if (answer === 0 || answer === 1) return answer;
     visited.fill(false);
   }
