d4c6ea6af0e041df5d225d2f804c0a6bc542e2e5
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-12-29 18:30:11 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 1978.45 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,51 +1,29 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
 function solution(arrayA, arrayB) {
-  let max = 0;
-  const arrayACopy = [...arrayA];
-  const arrayBCopy = [...arrayB];
-  const aMin = arrayACopy.sort((x, y) => x - y)[0];
-  const bMin = arrayBCopy.sort((x, y) => x - y)[0];
-  const aAliquot = new Set();
-  const bAliquot = new Set();
-  /*
-1. 각각 제일 작은 숫자의 약수 구하기
-1.5 공통 약수 제거하기
-2. 각 배열도 나눠떨어지는지 확인
-*/
+    const aResult = getAnswer(arrayA, arrayB)
+    const bResult = getAnswer(arrayB, arrayA)
 
-  const getAliquot = (num, set) => {
-    for (let i = 1; i * i <= num; ++i)
-      if (num % i === 0) {
-        set.add(i);
-        if (i * i !== num && num % (num / i) === 0) set.add(num / i);
-      }
-  };
+    return aResult > bResult ? aResult : bResult
+}
 
-  getAliquot(aMin, aAliquot);
-  getAliquot(bMin, bAliquot);
-
-  for (const val of aAliquot) {
-    if (bAliquot.has(val)) {
-      continue;
+function getAnswer (A, B) {
+    A.sort((a, b) => a - b)
+    for (let i = A[0]; i > 1; i--) {
+        if (A.every(a => a % i === 0) && !B.some(b => b % i === 0)) return i
     }
-
-    if (
-      arrayACopy.every((elem) => elem % val === 0) &&
-      arrayBCopy.every((elem) => elem % val !== 0)
-    )
-      max = Math.max(max, val);
-  }
-
-  for (const val of bAliquot) {
-    if (aAliquot.has(val)) {
-      continue;
-    }
-
-    if (
-      arrayACopy.every((elem) => elem % val !== 0) &&
-      arrayBCopy.every((elem) => elem % val === 0)
-    )
-      max = Math.max(max, val);
-  }
-
-  return max;
+    return 0
 }
