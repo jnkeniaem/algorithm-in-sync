eb884321838c8e4ff2c15812e53a9c7de448d910
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2025-12-29 19:01:34 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 103.08 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -4,38 +4,32 @@ function solution(arrayA, arrayB) {
   const arrayBCopy = [...arrayB];
   const aMin = arrayACopy.sort((x, y) => x - y)[0];
   const bMin = arrayBCopy.sort((x, y) => x - y)[0];
-  const aCommonAliquot = new Set();
-  const bCommonAliquot = new Set();
 
-  /*
-1. 각각 제일 작은 숫자의 약수 구하기
-1.5 공통 약수 제거하기
-2. 각 배열도 나눠떨어지는지 확인
-*/
-
-  const getAliquot = (num, set, array, anotherArray) => {
-    for (let i = 1; i * i <= num; ++i) {
-      if (
-        num % i === 0 &&
-        array.every((elem) => elem % i === 0) &&
-        anotherArray.every((elem) => elem % i !== 0)
-      ) {
-        set.add(i);
-        max = Math.max(max, i);
-      }
-      if (
-        num % (num / i) === 0 &&
-        array.every((elem) => elem % (num / i) === 0) &&
-        anotherArray.every((elem) => elem % (num / i) !== 0)
-      ) {
-        set.add(num / i);
-        max = Math.max(max, num / i);
-      }
-    }
+  const isCommonAliquot = (num, array, divisor) => {
+    return num % divisor === 0 && array.every((elem) => elem % divisor === 0);
   };
 
-  getAliquot(aMin, aCommonAliquot, arrayA, arrayB);
-  getAliquot(bMin, bCommonAliquot, arrayB, arrayA);
+  const getCommonAliquot = (num, array) => {
+    const aliquot = [];
+
+    for (let i = 1; i * i <= num; ++i) {
+      if (isCommonAliquot(num, array, i)) aliquot.push(i);
+      if (isCommonAliquot(num / i, array, num / i)) aliquot.push(num / i);
+    }
+
+    return aliquot;
+  };
+
+  const aAliquot = getCommonAliquot(aMin, arrayA);
+  const bAliquot = getCommonAliquot(bMin, arrayB);
+
+  for (const num of aAliquot) {
+    if (arrayB.every((elem) => elem % num !== 0)) max = Math.max(max, num);
+  }
+
+  for (const num of bAliquot) {
+    if (arrayA.every((elem) => elem % num !== 0)) max = Math.max(max, num);
+  }
 
   return max;
 }
