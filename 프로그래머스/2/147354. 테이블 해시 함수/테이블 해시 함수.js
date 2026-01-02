cfdba1b7af01505197e7e0cfb3c5f47f39e78008
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2026-01-02 15:52:02 +0900

    [level 2] Title: 테이블 해시 함수, Time: 18.35 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,29 +1,13 @@
 function solution(data, col, row_begin, row_end) {
-  const sortedData = [...data].map((elem) => [...elem]);
-  //   const S = new Array(row_end - row_begin + 1).fill(0);
+  const sortedData = [...data].sort(
+    (x, y) => x[col - 1] - y[col - 1] || y[0] - x[0]
+  );
 
-  //   정렬
-  sortedData.sort((x, y) => {
-    const subtraction = x[col - 1] - y[col - 1];
-
-    if (subtraction === 0)
-      // 첫 번째 컬럼의 값을 기준으로 내림차순 정렬
-      return y[0] - x[0];
-    return subtraction;
-  });
-
-  //   let answer = S[0];
-  //   for (let j = 1; j < S.length; ++j) {
-  // 	answer ^= S[j];
-  //   }
   let answer = 0;
 
-  for (let i = row_begin - 1; i < row_end; ++i) {
-    // i 번째 행의 튜플에 대해 각 컬럼의 값을 i 로 나눈 나머지들의 합
-    // S[i - row_begin + 1] = sortedData[i].reduce((acc, cur) => {
-    //   acc + (cur % (i + 1));
-    // }, 0);
-    answer ^= sortedData[i].reduce((acc, cur) => acc + (cur % (i + 1)), 0);
+  for (let i = row_begin; i <= row_end; ++i) {
+    const S_i = sortedData[i - 1].reduce((acc, cur) => acc + (cur % i), 0);
+    answer ^= S_i;
   }
 
   return answer;
