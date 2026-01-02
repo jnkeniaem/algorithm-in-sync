43bd8969770775906ab62ce3a35338225b152431
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2026-01-02 15:43:37 +0900

    [level 2] Title: 테이블 해시 함수, Time: 14.04 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,31 +1,29 @@
 function solution(data, col, row_begin, row_end) {
   const sortedData = [...data].map((elem) => [...elem]);
-  const s = new Array(row_end - row_begin + 1).fill(0);
+  //   const S = new Array(row_end - row_begin + 1).fill(0);
 
   //   정렬
   sortedData.sort((x, y) => {
-    const subtraction = x[col - 1] - y[col - 1]; // 뺀 결과
+    const subtraction = x[col - 1] - y[col - 1];
 
-    if (subtraction === 0) {
+    if (subtraction === 0)
       // 첫 번째 컬럼의 값을 기준으로 내림차순 정렬
       return y[0] - x[0];
-    } else return subtraction;
+    return subtraction;
   });
 
-  //   console.log(sortedData);
+  //   let answer = S[0];
+  //   for (let j = 1; j < S.length; ++j) {
+  // 	answer ^= S[j];
+  //   }
+  let answer = 0;
 
   for (let i = row_begin - 1; i < row_end; ++i) {
     // i 번째 행의 튜플에 대해 각 컬럼의 값을 i 로 나눈 나머지들의 합
-    for (let k = 0; k < sortedData[0].length; ++k) {
-      s[i - row_begin + 1] += sortedData[i][k] % (i + 1);
-    }
-  }
-
-  //   console.log(s);
-  let answer = s[0];
-
-  for (let j = 1; j < s.length; ++j) {
-    answer ^= s[j];
+    // S[i - row_begin + 1] = sortedData[i].reduce((acc, cur) => {
+    //   acc + (cur % (i + 1));
+    // }, 0);
+    answer ^= sortedData[i].reduce((acc, cur) => acc + (cur % (i + 1)), 0);
   }
 
   return answer;
