a2df21d65247aef8ee917aba85df92e097cb5bbb
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2026-01-13 15:44:02 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.25 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,44 +1,30 @@
 function solution(n, k) {
   const permutation = [];
-  const visited = new Array(n).fill(false);
-  const answer = [];
   const people = new Array(n).fill(0).map((_, idx) => idx + 1);
+
   const getFactorial = (num) => {
     let factorial = 1;
 
-    for (let i = num; i >= 1; --i) factorial *= i;
+    for (let i = 2; i <= num; ++i) factorial *= i;
 
     return factorial;
   };
-  /*
-5 = 3! * 0 + 2! * 2 + 1! * 1
-// 1 2 3 4
-// 2 3 4
-// 2 3
-// 3
-// 1 4 2 3
-6 = 3! * 0 + 2! * 2 + 1! * 2
-10 = 3! * 1 + 2! * 1 + 1! * 2
-// 1 2 3 4
-// 1 3 4
-// 1 4
-// 1
-// 2 3 4 1
-*/
 
-  let toSubstract = k;
-  for (let i = n - 1; i > 1; --i) {
-    const quota = Math.floor((toSubstract - 1) / getFactorial(i));
-    const person = people[quota];
+  let cur = k;
+
+  for (let i = n - 1; i >= 2; --i) {
+    const factorial = getFactorial(i);
+    const quota = Math.floor((cur - 1) / factorial);
+    if (quota) cur -= factorial * quota;
+    permutation.push(people[quota]);
     people.splice(quota, 1);
-    // answer.push(quota + 1); // (quota + 1)번째 그룹
-    answer.push(person); // (quota + 1)번째 그룹
-    toSubstract -= getFactorial(i) * quota;
   }
 
-  answer.push(people[toSubstract - 1]);
-  people.splice(toSubstract - 1, 1);
-  answer.push(people[0]);
+  permutation.push(people[cur - 1]);
+  permutation.push(people[2 - cur]);
 
-  return answer;
+  // people.splice(cur - 1, 1);
+  // permutation.push(people[0]);
+
+  return permutation;
 }
