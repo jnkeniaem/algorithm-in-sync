abecf5319ce999523b4c25b04744cf67f7378b4a
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2026-01-13 16:07:20 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.18 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,20 +1,18 @@
 function solution(n, k) {
   const permutation = [];
   const people = new Array(n).fill(0).map((_, idx) => idx + 1);
+  const factorials = new Array(n + 1).fill(1); // factorials[2] = 2!
 
-  const getFactorial = (num) => {
-    let factorial = 1;
-
-    for (let i = 2; i <= num; ++i) factorial *= i;
-
-    return factorial;
-  };
+  for (let i = 2; i <= n; ++i) factorials[n] *= i;
 
   let cur = k;
 
   for (let i = n - 1; i >= 2; --i) {
-    const factorial = getFactorial(i);
+    factorials[i] = Math.floor(factorials[i + 1] / (i + 1));
+
+    const factorial = factorials[i];
     const quota = Math.floor((cur - 1) / factorial);
+
     if (quota) cur -= factorial * quota;
     permutation.push(people[quota]);
     people.splice(quota, 1);
@@ -22,9 +20,6 @@ function solution(n, k) {
 
   permutation.push(people[cur - 1]);
   permutation.push(people[2 - cur]);
-
-  // people.splice(cur - 1, 1);
-  // permutation.push(people[0]);
 
   return permutation;
 }
