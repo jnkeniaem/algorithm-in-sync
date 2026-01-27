64b1e80c165c56863b4e05e199773f86d6ddf6f3
jeekim <80810728+jnkeniaem@users.noreply.github.com>
2026-01-27 16:14:41 +0900

    Time: 0 ms (100%), Space: 54.3 MB (65.82%) - LeetHub

--- LeetCode/0088-merge-sorted-array/0088-merge-sorted-array.js
+++ LeetCode/0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -7,7 +7,6 @@
  */
 var merge = function (nums1, m, nums2, n) {
   let end1 = m + n - 1;
-  // let end2 = n - 1;
   let cur1 = m - 1;
   let cur2 = n - 1;
 
@@ -16,34 +15,23 @@ var merge = function (nums1, m, nums2, n) {
     return;
   }
   if (m === 0) {
-    // nums1 = nums2;
-    // console.log(nums1);
     for (let i = 0; i < n; ++i) {
       nums1[i] = nums2[i];
     }
+    // console.log(nums1);
     return;
   }
 
   while (cur2 >= 0) {
-    if (nums1[cur1] <= nums2[cur2]) {
+    if (cur1 >= 0 && nums1[cur1] > nums2[cur2]) {
+      nums1[end1] = nums1[cur1];
+      cur1--;
+    } else {
       nums1[end1] = nums2[cur2];
       cur2--;
-    } else {
-      nums1[end1] = nums1[cur1];
-      if (cur1 === 0) {
-        nums1[cur1] = 0;
-      } else cur1--;
-      // nums1의 요소만 이동할뿐, 채워지지 않았다.
     }
     end1--;
   }
-  /*
-  - nums1 m-1, nums2 n-1 비교
-    - nums2 ≥ nums1
-        - nums2 요소를 nums1의 0중 맨 뒤에 위치 시키기
-    - nums2 < nums1
-        - nums1 요소를 nums1의 0중 맨 뒤에 위치 시키기
-- 각 반복문마다 nums1, nums2 중 움직인 요소의 포인터 앞으로 땡기기
-  */
+
   // console.log(nums1);
 };
