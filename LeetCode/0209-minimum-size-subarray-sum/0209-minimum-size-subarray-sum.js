65cc8b046e86bb595ee12368988bedf4531af224
jnkeniaem <jeanne.kim9@gmail.com>
2026-02-24 11:59:14 +0900

    Time: 3ms (65.96%)

--- LeetCode/0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ LeetCode/0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -6,30 +6,17 @@
 var minSubArrayLen = function (target, nums) {
   let minLen = 100001;
   let left = 0;
-  let right = 0;
-  let sum = nums[0];
-  const len = nums.length;
+  let sum = 0;
 
-  while (left < len && right < len && left <= right) {
-    if (target <= sum) {
+  for (let right = 0; right < nums.length; ++right) {
+    sum += nums[right];
+
+    while (target <= sum) {
       minLen = Math.min(minLen, right - left + 1);
       sum -= nums[left];
       left++;
-    } else {
-      right++;
-      sum += nums[right];
     }
   }
 
   return minLen === 100001 ? 0 : minLen;
 };
-
-let target = 7,
-  nums = [2, 3, 1, 2, 4, 3];
-console.log(minSubArrayLen(target, nums)); // 2
-
-((target = 4), (nums = [1, 4, 4]));
-console.log(minSubArrayLen(target, nums)); // 1
-
-((target = 11), (nums = [1, 1, 1, 1, 1, 1, 1, 1]));
-console.log(minSubArrayLen(target, nums)); // 0
