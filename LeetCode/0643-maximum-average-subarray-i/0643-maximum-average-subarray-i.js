d4171af13728daaea84b2bf6127054fbe4198ad3
jnkeniaem <jeanne.kim9@gmail.com>
2026-02-26 13:57:10 +0900

    Time: 0ms(100%)

--- LeetCode/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ LeetCode/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -5,22 +5,16 @@
  */
 var findMaxAverage = function (nums, k) {
   const len = nums.length;
-
-  if (len < k) return 0;
-
   let sum = 0;
 
   for (let i = 0; i < k; ++i) sum += nums[i];
 
-  let max = Number.MIN_SAFE_INTEGER; //
+  let max = sum;
 
-  for (let right = k - 1; right < len; ++right) {
+  for (let right = k; right < len; ++right) {
+    sum += nums[right];
+    sum -= nums[right - k];
     max = Math.max(max, sum);
-
-    if (right !== len - 1) {
-      sum -= nums[right - k + 1];
-      sum += nums[right + 1];
-    }
   }
 
   return max / k;
